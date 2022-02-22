4d2851935b2ea579ff05c2d57b757cd80cb7f57f
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2022-02-22 12:15:50 +0100

    Patching batch file again

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 :: Installation des composants nécessaires à l'utilisation du script SQL_to_CSV.
 TITLE Install SQL_to_CSV script components
-SET /p InstallPython= "Voulez vous installer Python 3.10 ? (Y/N) "
-IF %InstallPython% == "Y" (
+SET /p InstallPython= "Voulez vous installer Python 3.10 ? (Y/N)"
+IF /i %InstallPython% == "Y" (
     ECHO Installation de python 3.10 en cours.
     powershell -command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe', 'C:/Tools/python-3.10.0-amd64.exe'); & c:\Tools\python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Tools\Python310; [Environment]::SetEnvironmentVariable('PATH', ${env:path} + ';C:\Tools\Python310', 'Machine')"
 ) ELSE (
